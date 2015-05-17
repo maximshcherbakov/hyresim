9f1f7a9a4f9a9ece9d7495a62948e0f8ade97a42
maximshcherbakov <maxim.shcherbakov@gmail.com>
2015-05-17 20:50:16 +0300

    Added weather station class

--- src/weatherStation.py
+++ src/weatherStation.py
@@ -24,4 +24,5 @@ class WeatherStation():
 
 
     def get_weather_conditions(self, datetime_):
+        # Here we need to obtain real values based on location and datetime
         return {"solar_irradiance_": 0.25, "outdoor_temperature": -15.2}
