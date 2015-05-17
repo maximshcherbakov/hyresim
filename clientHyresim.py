6ca87c4f5dffeaefa78c3d375c5490cb398aa61e
maximshcherbakov <maxim.shcherbakov@gmail.com>
2015-05-17 21:40:51 +0300

    Append htmlLogger class for logging information into HTML file.

--- clientHyresim.py
+++ clientHyresim.py
@@ -4,14 +4,21 @@ from src.simulator import Simulator
 from src.hres import HRES
 from src.solarPanel import SolarPanel
 from src.weatherStation import WeatherStation
+from src.htmlLogger import htmlLogger
 import matplotlib.pyplot as plt
 from datetime import datetime, date, time, timedelta
+import os
 
 print ("Simulation of Hybrid Renewable Energy Systems")
 location = [59.950637, 30.305097]
 datetime_simulation_start = datetime(2014, 9, 1, 00, 15)
 iteration_timedelta = timedelta(minutes=15)
 number_of_iterations = 96
+path = os.path.dirname(os.path.realpath(__file__))
+
+lg = htmlLogger(path + '\\report\\hyresim.html')
+lg.appendParagraph("Simulation of Hybrid Renewable Energy Systems")
+
 # Create the weather station
 ws = WeatherStation(location)
 # Create the HRES
@@ -21,25 +28,15 @@ components_of_HRES.append(SolarPanel("Solar Panel # 5147485", 250))
 components_of_HRES.append(SolarPanel("Solar Panel # 5147486", 250))
 hres = HRES(components_of_HRES, location)
 # default_HRES.print_components_list()
-
-# Make a simulation of created HRES
-# Simulator.simulate(default_HRES, 100)
-# sim = Simulator("1", "2", 100)
-# sim.simulate(default_HRES)
 description, simulation_matrix = Simulator.simulate(hres, ws, datetime_simulation_start, iteration_timedelta, number_of_iterations)
 
+lg.appendDataFrame(simulation_matrix)
+
 print(description)
-print(simulation_matrix)
+# print(simulation_matrix)
 
-# tmp = simulation_matrix[1]
-# Visualize the simulation results
-# print(simulation_matrix.shape[1])
-# for i in range(1, simulation_matrix.shape[1]):
-#     plt.plot(simulation_matrix[:, i])
-
-# plt.plot(simulation_matrix[:, 1])
 plt.plot(simulation_matrix.iloc[:,2])
-plt.show()
-
-
-
+plt.plot(simulation_matrix.iloc[:,3])
+plt.savefig(path + '\\report\\demo.png')
+lg.appendImage('demo', path + '\\report\\demo.png')
+# plt.show()
---
+++ src/htmlLogger.py
@@ -0,0 +1,62 @@
+"""
+    HTML Logger allows to log all remarks into HTML
+"""
+__author__ = 'maxim.shcherbakov'
+
+import pandas as pd
+
+class htmlLogger():
+    """
+    Attributes
+    ----------
+    file_name : String
+        Name of the file for logging
+
+    file_pointer : pointer
+        Pointer to open file
+
+    """
+    file_name = ''
+    file_pointer = None
+
+    def __init__(self, name):
+        """
+        Create the pointer to file
+
+        :param
+        name : String
+            File name of logger.
+
+        """
+        self.name = name
+        try:
+            self.file_pointer = open(self.name, 'w')
+            self.file_pointer.write('<html>')
+        except:
+            print("Error occurs in attempt of file open")
+
+    def __del__(self):
+        self.file_pointer.write('</html>')
+        self.file_pointer.close()
+
+    def appendImage(self, _image_name, _image_file):
+        """
+        Append image into log file
+
+        :param
+        _image_name : String
+            Name of the image
+
+        :param
+        _image_file : String
+            Path and filename of image stored on the disk
+
+        :return:
+        """
+        self.file_pointer.write("<img src=\"" + _image_file + "\" alt=\"" + _image_name + "\">")
+
+    def appendParagraph(self, _message):
+        self.file_pointer.write("<p>" + _message + "</p>")
+
+    def appendDataFrame(self, dataframe_):
+        self.file_pointer.write(dataframe_.to_html())
