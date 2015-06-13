4b07a0d86cb96708634feb9539d553a2314a4228
maximshcherbakov <maxim.shcherbakov@gmail.com>
2015-06-13 11:43:15 +0300

    Add ConsumersFactory for creating typical consumers based on daily profiles

--- clientHyresim.py
+++ clientHyresim.py
@@ -12,7 +12,7 @@ from solarPanel import SolarPanel
 from weatherStation import WeatherStation
 from htmlLogger import htmlLogger
 from consumer import Consumer
-
+from consumer import ConsumersFactory
 from datagen import WeatherDataGenerator
 
 # Settings for simulation process
@@ -31,21 +31,22 @@ lg.appendParagraph("Simulation of Hybrid Renewable Energy Systems")
 ws = WeatherStation(location)
 
 # Create the HRES
+
+# Create consumers
+cf = ConsumersFactory()
+total_consumption = cf.make_consumer("Total Consumption", 1000, datetime_simulation_start, iteration_timedelta, number_of_iterations)
+# kitchen = cf.make_consumer("kitchen", 1000, datetime_simulation_start, iteration_timedelta, number_of_iterations)
+
 # solar panel (name_, nominal_power_capacity_, temperature_coefficient_)
-fridge = Consumer("Main Building", 100)
+# fridge = Consumer("Main Building", 100, None)
 
-# Define consumption within all period
-tmp_dates = []
-current_datetime = datetime_simulation_start
-for i in range(number_of_iterations):
-    tmp_dates.append(current_datetime)  # .strftime('%y-%m-%d %hour:%minutes')
-    current_datetime += iteration_timedelta
 
-print (tmp_dates)
 
 
 solar_panel = SolarPanel("Solar Panel # 001", 800, 0.02)
-components_of_HRES = [solar_panel, fridge]
+solar_panel2 = SolarPanel("Solar Panel # 002", 250, 0.02)
+
+components_of_HRES = [total_consumption, solar_panel, solar_panel2]
 
 
 #sp = SolarPanel("Thangs Solar Panel", 100)
@@ -61,19 +62,27 @@ lg.append_list_of_paragraphs(hres.get_description())
 description, simulation_matrix = Simulator.simulate(hres, ws, datetime_simulation_start, iteration_timedelta, number_of_iterations)
 
 
+columns = simulation_matrix.columns
+print(columns)
 
-lg.appendDataFrame(simulation_matrix)
-print(description)
+
+print(simulation_matrix[columns[1]][1])
+
+
 # print(simulation_matrix)
 
 # plt.plot(simulation_matrix.iloc[:,2])
 # plt.plot(simulation_matrix.iloc[:,3])
-# plt.plot(simulation_matrix["Solar Panel # 001"])
-# plt.plot(simulation_matrix["Main Building"])
-#
-# plt.savefig(path + '\\report\\demo.png')
-# lg.appendImage('demo', path + '\\report\\demo.png')
-# plt.show()
 
+plt.figure(num=None, figsize=(2, 2), dpi=80, facecolor='w', edgecolor='k')
+plt.plot(simulation_matrix["Total Consumption"])
+plt.savefig(path + '\\report\\demo.png')
+lg.appendImage('demo', path + '\\report\\demo.png')
+
+
+lg.appendDataFrame(simulation_matrix)
+print(description)
+
+print("Simulation is over")
  #wdg = WeatherDataGenerator()
  #print(wdg.generate())
--- consumer.py
+++ consumer.py
@@ -11,36 +11,57 @@ class Consumer(Component):
 
     """
 
-    power = 0
-    typical_daily_on_off_profile = []
+    power = 0   # maximal power of the component
+    # the Pandas Series of consumption during the simulation period.
+    # Indecies indicates the timestamps, and the data is energy consumed during the timedelta
+    # Column for data is named 'Consumption'
+    consumption_profile = None
 
-    def __init__(self, name_, power_):
+    def __init__(self, name_, power_, consumption_profile_):
         super().__init__(name_)
         self.power = power_
-    #     Init the typical_daily_profile
-        self.typical_daily_on_off_profile =  pd.DataFrame(np.zeros((96,2)))
-        t = datetime(2011,1,1,0, 15)
-        iteration_timedelta = timedelta(minutes=15)
-
-        for i in range(len(self.typical_daily_on_off_profile)):
-            self.typical_daily_on_off_profile.iloc[i, 0] = t
-            t = t + iteration_timedelta
-        for i in range(5):
-            self.typical_daily_on_off_profile.iloc[40 + i, 1] = 1
-
-        # print(typical_daily_on_off_profile)
+        self.consumption_profile = consumption_profile_
 
     def get_state(self, current_datetime_, **kwargs):
         # todo: max shcherbakov develop the simulation of consumer
         try:
-            hour = current_datetime_.hour
-            minute = current_datetime_.minute
-            # create index using the current_datetime_ and pickup the on/off status from typical_daily_on_off_profile
-            t = datetime(2011,1,1,hour, minute)
-            # print(self.typical_daily_on_off_profile[t])
-            self.state = random.random()
+            # Try to get the consumption for current _datetime_ in the consumption_profile
+            self.state = self.consumption_profile['Consumption'][current_datetime_]
         except:
             print("Error occurs in Component:Consumer. get_state")
-
+            self.state = random.random()
         return self.state
 
+
+class ConsumersFactory():
+    """
+        Creates the instance of the Class Consumer
+    """
+
+    def make_consumer(self, name_, power_, datetime_simulation_start, iteration_timedelta_, number_of_iterations_):
+        # consumption_profile = []
+        data_ = np.zeros(number_of_iterations_)
+        # Define consumption within all period
+        tmp_dates = []
+        current_datetime = datetime_simulation_start
+        for i in range(number_of_iterations_):
+            tmp_dates.append(current_datetime)
+            current_datetime += iteration_timedelta_
+        consumption_profile = pd.DataFrame(data=data_, index=tmp_dates, columns=['Consumption'])
+        # Randomly set up the values
+        for i in range(number_of_iterations_):
+            consumption_profile['Consumption'][i] =  25
+        for i in range(36, 46):
+            consumption_profile['Consumption'][i] =  150
+        for i in range(46, 57):
+            consumption_profile['Consumption'][i] =  100
+        for i in range(57, 72):
+            consumption_profile['Consumption'][i] =  120
+        for i in range(72, 84):
+            consumption_profile['Consumption'][i] =  100
+
+
+        print(consumption_profile)
+        # Create the consumer instance
+        consumer = Consumer(name_, power_, consumption_profile)
+        return consumer
